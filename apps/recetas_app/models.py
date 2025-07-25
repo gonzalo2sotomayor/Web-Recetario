8ac8d5197743803d46dd554b8b2034594288cbcb
AinoHana <rossyuchiha@gmail.com>
2025-07-25 08:41:13 -0300

    Añadido resultado de búsquedas
    - Nuevo HTML template para las búsquedas recetas_app.
    - Soporte para búsqueda simple y avanzada
    - Incluida grid layout para las recetas, img, titulo y descripción
    - Botones para navegación y búsqueda
    - Cambio de apariencia
    - Agregado de dropdown con JS porque el anterior no permitía clicar

--- apps/recetas_app/models.py
+++ apps/recetas_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Receta(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='recetas/', null=True, blank=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
