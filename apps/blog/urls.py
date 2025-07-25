2f5b1f3a157b7896179fcb509f24a9d2997e32e6
AinoHana <rossyuchiha@gmail.com>
2025-07-25 06:59:28 -0300

    HTML  y CSS actualizado

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os # Necesario para os.path.join
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.recetas_app.urls')), # Incluye las URLs de tu app de recetas
-    path('usuarios/', include('apps.usuarios.urls')), # Incluye las URLs de tu app de usuarios
+    path('', include('apps.recetas_app.urls')), # Incluye las URLs de la app de recetas
+    path('usuarios/', include('apps.usuarios.urls')), # Incluye las URLs de la app de usuarios
 ]
 
 # Configuración para servir archivos estáticos y de medios en desarrollo
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static')) #Más directo que el anterior
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
--- apps/recetas_app/urls.py
+++ apps/recetas_app/urls.py
@@ -6,4 +6,7 @@ app_name = 'recetas_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('receta/<int:pk>/', views.detalle_receta, name='detalle_receta'),
+    path('buscar/', views.simple_search_view, name='simple_search'), # Vista para la búsqueda simple
+    path('buscar/avanzada/', views.advanced_search_view, name='advanced_search'), # Nueva vista para el formulario
+    path('buscar/avanzada/resultados/', views.advanced_search_results_view, name='advanced_search_results'), # Vista para procesar el formulario
 ]
